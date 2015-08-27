02526cab22ea627d23e5587566f0976e13a024c5
richard chen <clq_web@126.com>
2015-08-27 17:30:12 +0800

    cmd demo init

---
+++ cmd/README.md
@@ -0,0 +1,4 @@
+# fiskit CMD规范 demo
+
+由于seajs的alias不支持多个模块指向同一文件，所以放弃js的部分打包，不过可以用seajs-combo进行合并请求。css文件部分打包没问题。
+
---
+++ cmd/fis-conf.js
@@ -0,0 +1,40 @@
+// 各种配置及开关
+var config = {
+    // 静态资源版本号
+    version: '1.0.0',
+    // 合并开关
+    packed: false,
+    // cdn域名开关，prod环境始终为true
+    cdn: false,
+    // cdn域名地址
+    cdnUrl: 'http://static.360buyimg.com/yiye',
+    // MD5后缀开关
+    useHask: false,
+    velocity: {
+        loader: 'seajs.use',
+        macro: '/page/macro.vm'
+    },
+    // 模块化配置
+    modules: {
+        mode: 'cmd',
+        forwardDeclaration: true,
+        baseUrl: '/'
+    }
+};
+
+fis.config.get('initConfig')(config);
+
+// 合并设置
+config.packed && fis
+    .match('/static/lib/**', {
+        packTo: '/static/lib/lib_pkg.js'
+    })
+    .match('/widget/**.{css,scss}', {
+        packTo: '/widget/widget_pkg.css'
+    })
+
+fis.media('dev').match('*', {
+    deploy: fis.plugin('local-deliver', {
+        to: 'd:\\www\\fiskit_test'
+    })
+});
