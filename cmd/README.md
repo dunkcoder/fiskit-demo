97bb535504e2cd57aef4455a281d72bbc97e7759
richard chen <clq_web@126.com>
2015-09-03 17:44:43 +0800

    增加模板发布

--- cmd/README.md
+++ cmd/README.md
@@ -2,3 +2,6 @@
 
 由于seajs的alias不支持多个模块指向同一文件，所以放弃js的部分打包，不过可以用seajs-combo进行合并请求。css文件部分打包没问题。
 
+## 前后端模板共用
+
+`fiskit release tmpl`只发布vm模板。由于`fis3-postpackager-loader`模块化加载依赖页面中带`data-loader`的`script`标签，所以将页面底部js引用放到`/page/_scripts.html`中，这样模板在单文件处理阶段`::stantard`就已经引入了js，在`::package`阶段即可被`fis3-packager-loader`识别。
--- cmd/fk-conf.js
+++ cmd/fk-conf.js
@@ -1,15 +1,16 @@
-var root = fiskit.project.getProjectPath();
+var util = fiskit.util,
+    root = fiskit.project.getProjectPath();
 
 // 各种配置及开关
 var config = {
     // 静态资源版本号
     version: '1.0.0',
     // 合并开关
-    packed: false,
+    packed: true,
     // cdn域名开关，prod环境始终为true
     cdn: false,
     // cdn域名地址
-    cdnUrl: 'http://static.360buyimg.com/newbie/yiyebs',
+    cdnUrl: '//static.360buyimg.com/newbie/yiyebs',
     // MD5后缀开关
     useHash: false,
     velocity: {
@@ -39,46 +40,25 @@ config.packed && fiskit
 
 fiskit.media('dev').match('*', {
     deploy: fiskit.plugin('local-deliver', {
-        to: 'd:\\www\\yiye_commerce'
+        to: 'd:\\www\\fiskit_test_cmd'
     })
 });
 
 // 只发布VM文件
+var tmpVelocity = util.merge({parse: false}, config.velocity);
 fiskit
     .media('tmpl')
-    .match('*', {
-        release: false
-    })
     .match('*.vm', {
-        parser: null,
-        isHtmlLike: false,
+        parser: fiskit.plugin('velocity', tmpVelocity),
         rExt: '.vm',
-        deploy: fiskit.plugin('local-deliver', {
-            to: './output/template/' + config.version
-        }),
-        postprocessor: [
-            // 将.scss|.less引用替换为css
-            function(content, file, settings) {
-                var rLink = /<link[^>]*(?:\/)?>/ig;
-                var rHref = /(href=("|')[^'"]+\.)(scss|less)/ig;
-                return content.replace(rLink, function(match) {
-                    return match.replace(rHref, function(match, p1, p2) {
-                        return [p1, 'css'].join('');
-                    });
-                });
-            },
-            // 将以/开头的script, img, link引用添加$!{staticDomain}前缀
-            function(content, file, settings) {
-                var rTag = /(<script[^>]*>)|(<link[^>]*>)|(<img[^>]*>)/ig;
-                var rSrcHref = /(\s(src|href)=('|"))(\/)([^\/]+)/ig;
-
-                return content = content.replace(rTag, function(match) {
-                    return match.replace(rSrcHref, function(match, p1, p2, p3, p4, p5) {
-                        //console.log( [p1, p2, p3, p4, p5].join('      '));
-                        return [p1, '$!{staticDomain}/', p5].join('');
-                    });
-                });
-            }
+        deploy: [
+            fiskit.plugin('replace', {
+                from: config.cdnUrl + '/' + config.version,
+                to: '$!{staticDomain}'
+            }),
+            fiskit.plugin('local-deliver', {
+                to: './output/template/' + config.version
+            })
         ]
     })
     .match('/page/(**.vm)', {
